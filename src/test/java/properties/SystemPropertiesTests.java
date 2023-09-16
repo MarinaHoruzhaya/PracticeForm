123cfe660a718ed79e92e3fdeffddf7edba4a907
Marina Horuzhaya <marinakh192@gmail.com>
2023-09-16 22:08:29 +0300

    properties added

--- src/test/java/properties/SystemPropertiesTests.java
+++ src/test/java/properties/SystemPropertiesTests.java
@@ -1,4 +1,25 @@
 package properties;
 
+import org.junit.jupiter.api.Test;
+
 public class SystemPropertiesTests {
+
+    @Test
+    void systemPropertiesTest() {
+        String browser = System.getProperty("browser");
+        System.out.println(browser);
+    }
+    @Test
+    void systemProperties1Test() {
+        System.setProperty("browser","chrome");
+        String browser = System.getProperty("browser");
+
+        System.out.println(browser);
+    }
+
+    @Test
+    void systemProperties2Test() {
+        String browser = System.getProperty("browser", "mozilla ");
+        System.out.println(browser);
+    }
 }
